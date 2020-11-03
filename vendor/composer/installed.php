7374b54a1b3469e8d843b470452d084bb33100b3
yamaputo-gospace <newstandardkenichi@gmail.com>
2020-11-03 21:54:52 +0900

    created composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e31d871d486f314fa26beb2a50f110b6d3822830',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e31d871d486f314fa26beb2a50f110b6d3822830',
+    ),
+  ),
+);
